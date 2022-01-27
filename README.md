db270a22725bdb0cd969238374a9146f6f164e58
Sougata Khan <sougat818@gmail.com>
2022-01-27 20:30:52 +1100

    LC 144 Binary Tree Preorder Traversal

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 [![Java CI with Gradle](https://github.com/sougat818/leetcode/actions/workflows/gradle.yml/badge.svg)](https://github.com/sougat818/leetcode/actions/workflows/gradle.yml)
 
-| # | Title | Solution | Difficulty |
-|---| ----- | -------- | ---------- |
-|1|[Two Sum](https://leetcode.com/problems/two-sum/)| [Java](java/two-sum/src/main/java/TwoSum.java)|Easy|
+| #   | Title                                                                    | Solution                                                                                   | Difficulty |
+|-----|--------------------------------------------------------------------------|--------------------------------------------------------------------------------------------| ---------- |
+| 1   | [Two Sum](https://leetcode.com/problems/two-sum/)                        | [Java](java/two-sum/src/main/java/TwoSum.java)                                             |Easy|
+| 144 | [Binary Tree Preorder Traversal](https://leetcode.com/problems/binary-tree-preorder-traversal/) | [Java](java/binary-tree-preorder-traversal/src/main/java/BinaryTreePreorderTraversal.java) |Easy|
 
---
+++ java/binary-tree-preorder-traversal/src/test/java/BinaryTreePreorderTraversalTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BinaryTreePreorderTraversalTest {
+
+    BinaryTreePreorderTraversal binaryTreePreorderTraversal = new BinaryTreePreorderTraversal();
+
+    private static Stream<Arguments> preorderTraversal() {
+        return Stream.<Arguments>builder()
+                .add(Arguments.of(List.of(1, 2, 3), new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null))))
+                .add(Arguments.of(List.of(), null))
+                .add(Arguments.of(List.of(1), new TreeNode(1, null, null)))
+                .build();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void preorderTraversal(List<Integer> expected, TreeNode root) {
+        List<Integer> actual = binaryTreePreorderTraversal.preorderTraversal(root);
+        assertTrue(actual.size() == expected.size() && actual.containsAll(expected) && expected.containsAll(actual));
+    }
+}
